3cc60af89380172f307f6f4fd431af32c56f9fde
Luca Cristian <40149892+LucaCristian@users.noreply.github.com>
2022-12-14 11:38:33 +0200

    Add files via upload

---
+++ OTL Hack/OTL Hack/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace OTL_Hack
+{
+    partial class Messages
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Messages));
+            this.Send_Button = new System.Windows.Forms.Button();
+            this.TextBox = new System.Windows.Forms.TextBox();
+            this.moveTimer = new System.Windows.Forms.Timer(this.components);
+            this.BottomBar = new System.Windows.Forms.PictureBox();
+            this.TopBar = new System.Windows.Forms.PictureBox();
+            this.delay = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.BottomBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Send_Button
+            // 
+            this.Send_Button.Location = new System.Drawing.Point(289, 509);
+            this.Send_Button.Name = "Send_Button";
+            this.Send_Button.Size = new System.Drawing.Size(43, 23);
+            this.Send_Button.TabIndex = 0;
+            this.Send_Button.Text = "Send";
+            this.Send_Button.UseVisualStyleBackColor = true;
+            this.Send_Button.Visible = false;
+            this.Send_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextBox
+            // 
+            this.TextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TextBox.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBox.Location = new System.Drawing.Point(114, 546);
+            this.TextBox.MaxLength = 100;
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(187, 16);
+            this.TextBox.TabIndex = 1;
+            this.TextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox_KeyPress);
+            // 
+            // moveTimer
+            // 
+            this.moveTimer.Interval = 20;
+            this.moveTimer.Tick += new System.EventHandler(this.moveTimer_Tick);
+            // 
+            // BottomBar
+            // 
+            this.BottomBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomBar.Image = global::OTL_Hack.Properties.Resources.BottomBar;
+            this.BottomBar.Location = new System.Drawing.Point(0, 538);
+            this.BottomBar.Name = "BottomBar";
+            this.BottomBar.Size = new System.Drawing.Size(344, 34);
+            this.BottomBar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BottomBar.TabIndex = 3;
+            this.BottomBar.TabStop = false;
+            // 
+            // TopBar
+            // 
+            this.TopBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBar.Image = global::OTL_Hack.Properties.Resources.TopBar2;
+            this.TopBar.Location = new System.Drawing.Point(0, 0);
+            this.TopBar.Name = "TopBar";
+            this.TopBar.Size = new System.Drawing.Size(344, 76);
+            this.TopBar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.TopBar.TabIndex = 2;
+            this.TopBar.TabStop = false;
+            // 
+            // delay
+            // 
+            this.delay.Enabled = true;
+            this.delay.Interval = 300;
+            this.delay.Tick += new System.EventHandler(this.messagedelay);
+            // 
+            // Messages
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(344, 572);
+            this.Controls.Add(this.Send_Button);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.BottomBar);
+            this.Controls.Add(this.TopBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Messages";
+            this.Text = "Messages";
+            ((System.ComponentModel.ISupportInitialize)(this.BottomBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TopBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Send_Button;
+        private System.Windows.Forms.TextBox TextBox;
+        private System.Windows.Forms.PictureBox MB1;
+        private System.Windows.Forms.Timer moveTimer;
+        private System.Windows.Forms.PictureBox TopBar;
+        private System.Windows.Forms.PictureBox BottomBar;
+        private System.Windows.Forms.Timer delay;
+    }
+}
+
---
+++ OTL Hack/OTL Hack/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Image = System.Drawing.Image;
+
+namespace OTL_Hack
+{
+    public partial class Messages : Form
+    {
+        //Anti Spam counter
+        int delaysecs = 0;
+        
+        // Labels and Picture Boxes Creation
+        Label MSG1 = new Label();
+        Label MSG2 = new Label();
+        Label MSG3 = new Label();
+
+        Label HMSG1 = new Label();
+        Label HMSG2 = new Label();
+        Label HMSG3 = new Label();
+
+        PictureBox MB2 = new PictureBox();
+        PictureBox MB3 = new PictureBox();
+        Bitmap image = Properties.Resources.MessageBubble;
+
+        PictureBox HMB1 = new PictureBox();
+        PictureBox HMB2 = new PictureBox();
+        PictureBox HMB3 = new PictureBox();
+        Bitmap Himage = Properties.Resources.MessageHimBubble;
+
+        /*
+        MBspeed = Message Bubble speed of scrolling
+        mNR = Number of my messages (on the screen)
+        mNR2 = Number of his messages (on the screen)
+        */
+        int MBspeed = 19;
+        int mNR = 0;
+        int mNR2 = 0;
+
+        public Messages()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Hud Order
+            this.TopBar.BringToFront();
+            this.BottomBar.BringToFront();
+            this.TextBox.BringToFront();
+
+            // Line Input
+            string textbox = TextBox.Text.ToString();
+
+            // Detect if the text box is empty
+            if (String.IsNullOrEmpty(TextBox.Text))
+                return;
+            if (delaysecs < 0)
+            {
+                TextBox.Text = String.Empty;
+                delaysecs = 1;
+
+                string line = "";
+
+                // Date And Time
+                var dateAndTime = DateTime.Now;
+                int h = dateAndTime.Hour;
+
+                // Random Letters
+                Random ran = new Random();
+                String b = "ABCDEFGHIJKLMNOPRSTQUVXWXYZ";
+                String random = "";
+                for (int i = 0; i < 4; i++)
+                {
+                    int a = ran.Next(26);
+                    random = random + b.ElementAt(a);
+                }
+
+                // Random Number
+                string nr = "";
+                Random rnd = new Random();
+                for (int j = 0; j < 4; j++)
+                {
+                    nr = nr + Convert.ToString(rnd.Next(10));
+                }
+
+                // Front End
+                mNR++;
+                mNR2++;
+
+                nrMes();
+                MBspeed = 19;
+
+                moveTimer.Enabled = true;
+                moveTimer_Tick(sender, e);
+
+                // Coordinates (H - his messages)
+                int xMSG = 294, yMSG = 537;
+                int HxMSG = 25, HyMSG = 585;
+
+                // Text Length Limit
+                if (textbox.Length == 1)
+                {
+                    line = textbox.Substring(0, 1);
+                }
+                if (textbox.Length > 1)
+                {
+                    line = textbox.Substring(0, 2);
+                    xMSG = xMSG - 5;
+                }
+
+
+                if (mNR2 == 1)
+                {
+                    MSG1.Location = new Point(xMSG, yMSG);
+                    MSG1.AutoSize = true;
+                    MSG1.ForeColor = Color.White;
+                    MSG1.BackColor = Color.FromArgb(103, 203, 105);
+                    MSG1.Font = new Font("Calibri", 13);
+                    this.Controls.Add(MSG1);
+                    MSG1.Text = line.ToString();
+                    this.MSG1.BringToFront();
+
+                    HMSG1.Location = new Point(HxMSG, HyMSG);
+                    HMSG1.AutoSize = true;
+                    HMSG1.ForeColor = Color.Black;
+                    HMSG1.BackColor = Color.FromArgb(233, 233, 235);
+                    HMSG1.Font = new Font("Calibri", 13);
+                    this.Controls.Add(HMSG1);
+                    HMSG1.Text = $"Biletul dvs pentru linia {line} a fost\nactivat. Cost 0.67Eur+Tva Cod\nconfirmare {random}{nr} Valabil pana\nla " + $"{h + 1}:" + DateTime.Now.ToString("mm") + " " + DateTime.Now.ToString("dd.MM.yyyy") + " Calatorie\nplacuta!";
+                    this.HMSG1.BringToFront();
+                }
+                if (mNR2 == 2)
+                {
+                    MSG2.Location = new Point(xMSG, yMSG);
+                    MSG2.AutoSize = true;
+                    MSG2.ForeColor = Color.White;
+                    MSG2.BackColor = Color.FromArgb(103, 203, 105);
+                    MSG2.Font = new Font("Calibri", 13);
+                    this.Controls.Add(MSG2);
+                    MSG2.Text = line.ToString();
+                    this.MSG2.BringToFront();
+
+                    HMSG2.Location = new Point(HxMSG, HyMSG);
+                    HMSG2.AutoSize = true;
+                    HMSG2.ForeColor = Color.Black;
+                    HMSG2.BackColor = Color.FromArgb(233, 233, 235);
+                    HMSG2.Font = new Font("Calibri", 13);
+                    this.Controls.Add(HMSG2);
+                    HMSG2.Text = $"Biletul dvs pentru linia {line} a fost\nactivat. Cost 0.67Eur+Tva Cod\nconfirmare {random}{nr} Valabil pana\nla " + $"{h + 1}:" + DateTime.Now.ToString("mm") + " " + DateTime.Now.ToString("dd.MM.yyyy") + " Calatorie\nplacuta!";
+                    this.HMSG2.BringToFront();
+                }
+                if (mNR2 == 3)
+                {
+                    MSG3.Location = new Point(xMSG, yMSG);
+                    MSG3.AutoSize = true;
+                    MSG3.ForeColor = Color.White;
+                    MSG3.BackColor = Color.FromArgb(103, 203, 105);
+                    MSG3.Font = new Font("Calibri", 13);
+                    this.Controls.Add(MSG3);
+                    MSG3.Text = line.ToString();
+                    this.MSG3.BringToFront();
+
+                    HMSG3.Location = new Point(HxMSG, HyMSG);
+                    HMSG3.AutoSize = true;
+                    HMSG3.ForeColor = Color.Black;
+                    HMSG3.BackColor = Color.FromArgb(233, 233, 235);
+                    HMSG3.Font = new Font("Calibri", 13);
+                    this.Controls.Add(HMSG3);
+                    HMSG3.Text = $"Biletul dvs pentru linia {line} a fost\nactivat. Cost 0.67Eur+Tva Cod\nconfirmare {random}{nr} Valabil pana\nla " + $"{h + 1}:" + DateTime.Now.ToString("mm") + " " + DateTime.Now.ToString("dd.MM.yyyy") + " Calatorie\nplacuta!";
+                    this.HMSG3.BringToFront();
+
+                    mNR2 = 0;
+                }
+            }
+        }
+
+        private void TextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                e.Handled = true; // Error Sound Removal
+                button1_Click(sender, e);
+            }
+        }
+
+        private void moveTimer_Tick(object sender, EventArgs e)
+        {
+            /*
+             * Assigning the speed of scrolling
+             * MB1, 2, 3 - first, second and third message bubble (picture box)
+             * HMB1, 2, 3 - - first, second and third his message bubble (picture box)
+            */
+            MB1.Top -= MBspeed;
+            MB2.Top -= MBspeed;
+            MB3.Top -= MBspeed;
+            HMB1.Top -= MBspeed;
+            HMB2.Top -= MBspeed;
+            HMB3.Top -= MBspeed;
+
+            /*
+             * MSG1, 2, 3 - first, second and third message (label)
+             * HMSG1, 2, 3 - - first, second and third his message (label)
+            */
+            MSG1.Top -= MBspeed;
+            MSG2.Top -= MBspeed;
+            MSG3.Top -= MBspeed;
+            HMSG1.Top -= MBspeed;
+            HMSG2.Top -= MBspeed;
+            HMSG3.Top -= MBspeed;
+            
+            // Movement Stop
+            if (MBspeed > 0)
+                MBspeed--;
+        }
+        private void nrMes()
+        {
+            // Coordinates (H - his messages)
+            int xMB = 280, yMB = 550;
+            int HxMB = 10, HyMB = 600;
+            
+            if (mNR == 1)
+            {
+                MB1 = new PictureBox();
+                Controls.Add(MB1);
+                MB1.Image = (Image)image;
+                MB1.SizeMode = PictureBoxSizeMode.StretchImage;
+                MB1.Top = yMB;
+                MB1.Left = xMB;
+                MB1.Width = 50;
+                MB1.Height = 35;
+
+                HMB1 = new PictureBox();
+                Controls.Add(HMB1);
+                HMB1.Image = (Image)Himage;
+                HMB1.SizeMode = PictureBoxSizeMode.StretchImage;
+                HMB1.Top = HyMB;
+                HMB1.Left = HxMB;
+                HMB1.Width = 290;
+                HMB1.Height = 120;
+            }
+            else if (mNR == 2)
+            {
+                MB2 = new PictureBox();
+                MB2.SizeMode = PictureBoxSizeMode.StretchImage;
+                MB2.Image = (Image)image;
+                Controls.Add(MB2);
+                MB2.Top = yMB;
+                MB2.Left = xMB;
+                MB2.Width = 50;
+                MB2.Height = 35;
+
+                HMB2 = new PictureBox();
+                Controls.Add(HMB2);
+                HMB2.Image = (Image)Himage;
+                HMB2.SizeMode = PictureBoxSizeMode.StretchImage;
+                HMB2.Top = HyMB;
+                HMB2.Left = HxMB;
+                HMB2.Width = 290;
+                HMB2.Height = 120;
+            }
+            else if (mNR == 3)
+            {
+                MB3 = new PictureBox();
+                Controls.Add(MB3);
+                MB3.SizeMode = PictureBoxSizeMode.StretchImage;
+                MB3.Image = (Image)image;
+                MB3.Top = yMB;
+                MB3.Left = xMB;
+                MB3.Width = 50;
+                MB3.Height = 35;
+
+                HMB3 = new PictureBox();
+                Controls.Add(HMB3);
+                HMB3.Image = (Image)Himage;
+                HMB3.SizeMode = PictureBoxSizeMode.StretchImage;
+                HMB3.Top = HyMB;
+                HMB3.Left = HxMB;
+                HMB3.Width = 290;
+                HMB3.Height = 120;
+            }
+
+            // Position Reset
+            if (MB1.Top<50)
+            {
+                MB1.Top = yMB;
+                MB1.Left = xMB;
+            }
+            else if (MB2.Top < 50)
+            {
+                MB2.Top = yMB;
+                MB2.Left = xMB;
+            }
+            else if (MB3.Top < 50)
+            {
+                MB3.Top = yMB;
+                MB3.Left = xMB;
+            }
+
+            if (HMB1.Top < 80)
+            {
+                HMB1.Top = HyMB;
+                HMB1.Left = HxMB;
+            }
+            else if (HMB2.Top < 80)
+            {
+                HMB2.Top = HyMB;
+                HMB2.Left = HxMB;
+            }
+            else if (HMB3.Top < 80)
+            {
+                HMB3.Top = HyMB;
+                HMB3.Left = HxMB;
+            }
+        }
+
+        private void messagedelay(object sender, EventArgs e)
+        {
+            delaysecs--;
+        }
+    }
+}
